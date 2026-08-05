a0d68eeb9afc0fd9bb1364edcc7ee90826b9ca32
jing <2453620238@qq.com>
2026-08-05 14:53:20 +0800

    chore: initialize security demo project

---
+++ src/test/java/com/jing/security/SecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jing.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
